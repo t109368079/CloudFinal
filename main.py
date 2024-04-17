f992ce20601a915d815382106edc6429e85ffcfc
ZHITING YEH <t109368079@ntut.org.tw>
2024-04-16 21:27:31 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 16 21:20:52 2024
+
+@author: garyy
+"""
+
+import os
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+st.title("Test")
